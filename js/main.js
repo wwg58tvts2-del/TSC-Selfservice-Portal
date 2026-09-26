08cd22019c6d884c478d159043b54bff90f5dca2
wwg58tvts2-del <wwg58tvts2@privaterelay.appleid.com>
2026-09-26 11:32:22 +0200

    Login

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import {createApp} from "https://unpkg.com/petite-vue?module";
-import {state} from "./state.js?v=20260926-native-member-login-4";
+import {state} from "./state.js?v=20260926-native-member-login-5";
 
 
 // Bestehende Form.io-Skripte rufen
